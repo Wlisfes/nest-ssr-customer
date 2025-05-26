a8b724acafb0526abf12f6378b825865bd0dfffa
妖雨纯 <876451336@qq.com>
2025-05-26 21:18:37 +0800

    ✨ feat(server): 生产环境新增compression插件

--- server/main.ts
+++ server/main.ts
@@ -3,14 +3,19 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@server/app.module'
 import { createViteServer } from '@server/vite.server'
 import { resolve } from 'path'
+import compression from 'compression'
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule, {
         cors: true
     })
-    const vite = await createViteServer()
-    await app.use(vite.middlewares)
-    app.useStaticAssets(resolve(__dirname, '../build/client'), { index: false })
+    if (process.env.NODE_ENV === 'production') {
+        app.useStaticAssets(resolve(__dirname, '../build/client'), { index: false })
+        app.use(compression())
+    } else {
+        const vite = await createViteServer()
+        await app.use(vite.middlewares)
+    }
     app.enableCors()
     return await app.listen(process.env.NODE_PORT).then(() => {
         console.log(`Nest服务启动[${process.env.NODE_ENV}]:`, `http://localhost:${process.env.NODE_PORT}`)
