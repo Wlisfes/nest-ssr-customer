1ff6dae8af4bce876ab14eb0400ac7a7166b6da4
妖雨纯 <876451336@qq.com>
2025-06-02 11:56:48 +0800

    ✨ feat(server): 完善api地址转发

--- server/app.module.ts
+++ server/app.module.ts
@@ -5,17 +5,14 @@ import { TransformInterceptor } from '@server/interceptor/transform.interceptor'
 import { HttpExceptionFilter } from '@server/filters/http-exception.filter'
 import { LoggerModule } from '@server/modules/logger/logger.module'
 import { DeployModule } from '@server/modules/deploy/deploy.module'
-import { AppController } from '@server/app.controller'
-import { AppService } from '@server/app.service'
+import { WebModule } from '@server/modules/web/web.module'
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: `.env.${process.env.NODE_ENV}` }), LoggerModule, DeployModule],
-    controllers: [AppController],
+    imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: `.env.${process.env.NODE_ENV}` }), LoggerModule, DeployModule, WebModule],
     providers: [
         { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
         { provide: APP_FILTER, useClass: HttpExceptionFilter },
-        ConfigService,
-        AppService
+        ConfigService
     ]
 })
 export class AppModule {}
--- server/modules/web/web.controller.ts
+++ server/modules/web/web.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Header, Request } from '@nestjs/common'
 import { createRouteServer } from '@server/vite.server'
 
-@Controller('*')
-export class AppController {
-    @Get()
+@Controller()
+export class WebController {
+    @Get('*')
     @Header('Content-Type', 'text/html')
     async fetchBaseRender(@Request() request) {
         try {
